85b54349be2b7121d2daefeb59c9214f4099765c
Michael Telahun Makonnen <miket@clearict.com>
2019-07-22 02:37:07 -0400

    Customize url

--- odoo/tools/config.py
+++ odoo/tools/config.py
@@ -75,7 +75,7 @@ class configmanager(object):
         self.options = {
             'admin_passwd': 'admin',
             'csv_internal_sep': ',',
-            'publisher_warranty_url': 'http://services.openerp.com/publisher-warranty/',
+            'publisher_warranty_url': 'http://',
             'reportgz': False,
             'root_path': None,
         }
